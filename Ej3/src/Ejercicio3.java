776c64355e0ace06db642c7c16dfb7666392c442
Albert Alonso Lopez <albalolop2@alu.edu.gva.es>
2023-12-01 00:27:28 +0100

    Segunda versión por Alberto Alonso López

--- Ej3/src/Ejercicio3.java
+++ Ej3/src/Ejercicio3.java
@@ -4,23 +4,29 @@ public class Ejercicio3 {
         String usuario_correcto = "Usuario";
         String contrasena_correcta = "contrasena123";
         String usuario, contrasena;
+        boolean salir=false;//Alberto: Declaración variable booleana.
         Scanner leer=new Scanner(System.in);
-        System.out.print("Introduzca su nombre de usuario: ");
-        usuario=leer.nextLine();
-        System.out.print("Introduzca su contrase\u00f1a: ");
-        contrasena=leer.nextLine();
+
+        do
+        {//Alberto: Bucle doWhile para repetir en caso de ser datos incorrectos
+            System.out.print("\nIntroduzca su nombre de usuario: ");
+            usuario=leer.nextLine();
+            System.out.print("Introduzca su contrase\u00f1a: ");
+            contrasena=leer.nextLine();
 
         //comprobar si el usuario y la contraseña son correctos
         //son la funcion equals comprobamos que dos cadenas son iguales,
 
         if (usuario_correcto.equals(usuario) && contrasena_correcta.equals(contrasena)){
             System.out.print ("¡Bienvenido, " + usuario +"!");
+            salir=true;
         }else if (!usuario_correcto.equals(usuario) && contrasena_correcta.equals(contrasena)){
             System.out.print ("Nombre de usuario incorrecto.");
         }else if (usuario_correcto.equals(usuario) && !contrasena_correcta.equals(contrasena)){
-            System.out.print ("Contrase\u00f1a incorrecta");
+            System.out.println ("Contrase\u00f1a incorrecta");
         }else {
-            System.out.print ("Acceso denegado");
+            System.out.println ("Acceso denegado");
         }
+        }while(!salir);
     }
 }
